08f075baf9a4f77b3c5d4a0a45d7238313465cb9
Alonso <alonso@alonsorobles.com>
2017-03-10 12:04:16 -0600

    Redirects

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ var React = require('react');
 var Router = require('react-router');
 var IndexRoute = Router.IndexRoute;
 var Route = Router.Route;
+var Redirect = Router.Redirect;
 
 var routes = (
     <Route path="/" component={require('./components/app')}>
         <IndexRoute component={require('./components/homePage')} />
         <Route path="authors" component={require('./components/authors/authorsPage')} />
         <Route path="about" component={require('./components/about/aboutPage')}/>
+        <Redirect from="about-us" to="about" />
+        <Redirect from="about/*" to="about" />
+        <Redirect from="awthors" to="authors" />
         <Route path="*" component={require('./components/notFoundPage')}/>
     </Route>
 );
